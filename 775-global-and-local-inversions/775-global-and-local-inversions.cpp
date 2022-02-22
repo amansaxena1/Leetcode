52c65a8321af18c7ad3af05d3eb852aaf3801404
Aman Saxena <65945317+amansaxena1@users.noreply.github.com>
2022-02-22 22:58:46 +0530

    Time: 166 ms (69.16%), Space: 83.4 MB (16.73%) - LeetHub

--- 775-global-and-local-inversions/775-global-and-local-inversions.cpp
+++ 775-global-and-local-inversions/775-global-and-local-inversions.cpp
@@ -1,27 +1,10 @@
 class Solution {
 public:
-    int global(vector<int>& nums, int a){
-        set<int>s;
-        int cnt = 0;
-        for(int i=nums.size()-1; i>=0; i--){
-            if(cnt>a) return -1;
-            s.insert(nums[i]);
-            auto it = s.find(nums[i]);
-            cnt += distance(s.begin(), it);
+    bool isIdealPermutation(vector<int>& A) {
+        for(int i = 0 ; i < A.size() ; i++){
+            if(abs(A[i] - i) > 1)
+                return false;
         }
-        return cnt;
-    }
-    int local(vector<int>& nums){
-        int cnt=0;
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i] > nums[i+1]) cnt++;
-        }
-        return cnt;
-    }
-    bool isIdealPermutation(vector<int>& nums) {
-        int b = local(nums);
-        int a = global(nums,b);
-        if(a==b) return true;
-        return false;
+        return true;
     }
 };
