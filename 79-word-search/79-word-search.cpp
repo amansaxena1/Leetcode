09442408d1b4f5f3d81a7b4e23d4ca2885cad9a8
Aman Saxena <65945317+amansaxena1@users.noreply.github.com>
2022-03-11 16:04:29 +0530

    Time: 352 ms (73.75%), Space: 8 MB (75.14%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,36 +1,28 @@
 class Solution {
-public:
-    
-    
+public:    
     bool check(int x, int y, int ind,  vector<vector<char>> &board, string word, bool status){
         if(status || ind == word.size()) return true;
         bool a = false;
-        
         if(x < board.size()-1 && board[x+1][y] == word[ind]) {
             board[x+1][y] = '*';
             a = check(x+1, y, ind+1, board, word, false);
             board[x+1][y] = word[ind];
-
         }
-            
         if(!a && x > 0 && board[x-1][y] == word[ind]){
             board[x-1][y] = '*';
             a = check(x-1, y, ind+1, board, word, false);
             board[x-1][y] = word[ind];
         }
-        
         if(!a && y > 0 && board[x][y - 1] == word[ind]){
             board[x][y-1] = '*';
             a = check(x, y - 1, ind+1, board, word, false);
             board[x][y-1] = word[ind];
         }
-        
         if(!a && y < board[0].size() - 1 && board[x][y + 1] == word[ind]) {
             board[x][y+1] = '*';
             a = check(x, y + 1, ind+1, board, word, false);
             board[x][y+1] = word[ind];
         }
-
         if(a){
             status = true;
             return true;
@@ -39,10 +31,8 @@ public:
     }
     
     bool exist(vector<vector<char>>& board, string word) {
-        
         for(int i=0;i<board.size();i++){
             for(int j=0;j<board[0].size();j++){
-                // char t = board[i][j];
                 if(board[i][j] == word[0]) {
                     board[i][j] = '*';
                     if(check(i, j, 1, board, word, false)) return true;
@@ -53,9 +43,3 @@ public:
         return false;
     }
 };
-// ["A","B","C","E"]
-// ["S","F","E","S"]
-// ["A","D","E","E"]
-// "ABCESEEEFS"
-
-
