98511fb7d43cf2b6a92515ed44d560e3d6437222
Aman Saxena <65945317+amansaxena1@users.noreply.github.com>
2022-04-08 00:20:10 +0530

    Added solution - LeetHub

--- Reach the Nth point - GFG/reach-the-nth-point.cpp
+++ Reach the Nth point - GFG/reach-the-nth-point.cpp
@@ -5,14 +5,29 @@ using namespace std;
  // } Driver Code Ends
 class Solution{
 	public:
+	    int helper(vector<int>&v, int n){
+	        if(n==1 || n==2){
+	            v[n] = n;
+	            return v[n];
+	        }
+	        if(v[n] != -1) return v[n];
+	        int res = 0;
+	        res += helper(v,n-1);
+	        res += helper(v,n-2);
+	        v[n] = (res) % (1000000007);
+	        return v[n];
+	    }
+	    
 		int nthPoint(int n){
-		    long long int arr[10001];
-		    arr[1] = 1;
-		    arr[2] = 2;
-		    for(int i=3;i<=n;i++){
-		        arr[i] = (arr[i-1] + arr[i-2])%1000000007;
-		    }
-		    return (int)arr[n];
+		    vector<int>v(n+1,-1);
+		    return helper(v,n);
+		  //  long long int arr[10001];
+		  //  arr[1] = 1;
+		  //  arr[2] = 2;
+		  //  for(int i=3;i<=n;i++){
+		  //      arr[i] = (arr[i-1] + arr[i-2])%1000000007;
+		  //  }
+		  //  return (int)arr[n];
 		}
 };
 
