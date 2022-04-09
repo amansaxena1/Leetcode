0a0513d39a1b427197cfee06d0bf57b238af45c5
Aman Saxena <65945317+amansaxena1@users.noreply.github.com>
2022-04-10 02:12:17 +0530

    Time: 4 ms, Memory: 6 MB - LeetHub

---
+++ hamming-distance/hamming-distance.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int z = x^y;
+        int cnt=0;
+        while(z){
+            z = z&(z-1);
+            cnt++;
+        }
+        return cnt;
+    }
+};
