ca2987144a49c53a83497666276abf58031e0d8c
Aman Saxena <65945317+amansaxena1@users.noreply.github.com>
2022-04-09 21:06:17 +0530

    Time: 46 ms (82.39%), Space: 31.7 MB (60.24%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -7,12 +7,8 @@ public:
         vector<int>ans;
         while(l<r){
             llint mid = (l+r)/2;
-            if(x - arr[mid] > arr[mid+k]-x){
-                l = mid+1;
-            }
-            else{
-                r = mid;
-            }
+            if(x - arr[mid] > arr[mid+k] - x) l = mid+1;
+            else r = mid;
         }
         for(llint i=0;i<k;i++) ans.push_back(arr[l+i]);
         return ans;
