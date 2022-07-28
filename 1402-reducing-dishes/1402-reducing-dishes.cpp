844e690b84ced4595813c31058cc6958982f615e
Aman Saxena <65945317+amansaxena1@users.noreply.github.com>
2022-07-28 07:02:34 +0530

    Time: 6 ms (71.53%), Space: 8.3 MB (44.05%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -4,10 +4,15 @@ public:
     int maxSatisfaction(vector<int>& satisfaction) {
         sort(satisfaction.begin(),satisfaction.end());
         llint ans = 0;
-        for(llint i=0;i<satisfaction.size();i++){
-            int temp = 0;
-            for(llint j=1;j<=satisfaction.size()-i;j++) temp += j*satisfaction[i+j-1];
-            ans = max(ans, temp);
+        vector<llint> v(satisfaction.size(), 0);
+        for(llint i=satisfaction.size()-1;i>=0;i--) {
+            v[i] = satisfaction[i];
+            if(i<satisfaction.size()-1) v[i] += v[i+1];
+        }
+        llint curr = 0;
+        for(llint i=satisfaction.size()-1;i>=0;i--){
+            curr += v[i];
+            ans = max(ans, curr);
         }
         return ans;
     }
